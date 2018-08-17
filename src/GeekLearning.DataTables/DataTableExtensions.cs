ccb827850c11ee5481adf98bc9a0973c85a2bc65
Anna Yafi <annayafi@gmail.com>
2018-08-17 12:07:36 +0930

    changes indentation

--- src/GeekLearning.DataTables/DataTableExtensions.cs
+++ src/GeekLearning.DataTables/DataTableExtensions.cs
@@ -1,69 +1,69 @@
 ﻿namespace System.Linq
 {
-    using GeekLearning.DataTables;
+        using GeekLearning.DataTables;
 
-    public static class DataTableExtensions
-    {
-        public static IQueryable<TData> Paginate<TData>(this IQueryable<TData> query, IDataTableContext<TData> context)
-            where TData : class
+        public static class DataTableExtensions
         {
-            if (context.Resolver == null)
-            {
-                throw new InvalidOperationException("A DataTable resolver is required to paginate.");
-            }
-
-            context.Query = query;
-            context.FilteredQuery = context.Query
-                .Order(context.Parameters.Order, context.Resolver)
-                .Search(context.Parameters.Search, context.Resolver);
-            context.PaginatedQuery = context.FilteredQuery
-                .Skip(context.Parameters.Start)
-                .Take(context.Parameters.Length);
-
-            return context.PaginatedQuery;
-        }
-
-        public static IQueryable<TData> Order<TData>(this IQueryable<TData> query, DataTableOrder[] orderParameters, DataTableResolver resolver)
-            where TData : class
-        {
-            if (resolver == null)
-            {
-                throw new InvalidOperationException("A DataTable resolver is required to order.");
-            }
-
-            for (int i = 0; i < orderParameters.Length; i++)
-            {
-                bool isDesc = orderParameters[i].Direction == DataTableOrderDirection.DESC;
-
-                var orderParameter = orderParameters[i];
-
-                var orderableColumn = string.IsNullOrEmpty(orderParameter.ColumnName) 
-                    ? resolver.OrderableColumns[orderParameters[i].Column] 
-                    : resolver.OrderableColumns.FirstOrDefault(oc => string.Compare(oc, orderParameter.ColumnName, true) == 0);
-
-                if (orderableColumn != null)
+                public static IQueryable<TData> Paginate<TData>(this IQueryable<TData> query, IDataTableContext<TData> context)
+                    where TData : class
                 {
-                    query = i == 0 ? query.DynamicOrderBy(orderableColumn, isDesc) : query.DynamicThenBy(orderableColumn, isDesc);
+                        if (context.Resolver == null)
+                        {
+                                throw new InvalidOperationException("A DataTable resolver is required to paginate.");
+                        }
+
+                        context.Query = query;
+                        context.FilteredQuery = context.Query
+                            .Order(context.Parameters.Order, context.Resolver)
+                            .Search(context.Parameters.Search, context.Resolver);
+                        context.PaginatedQuery = context.FilteredQuery
+                            .Skip(context.Parameters.Start)
+                            .Take(context.Parameters.Length);
+
+                        return context.PaginatedQuery;
                 }
-            }
 
-            return query;
+                public static IQueryable<TData> Order<TData>(this IQueryable<TData> query, DataTableOrder[] orderParameters, DataTableResolver resolver)
+                    where TData : class
+                {
+                        if (resolver == null)
+                        {
+                                throw new InvalidOperationException("A DataTable resolver is required to order.");
+                        }
+
+                        for (int i = 0; i < orderParameters.Length; i++)
+                        {
+                                bool isDesc = orderParameters[i].Direction == DataTableOrderDirection.DESC;
+
+                                var orderParameter = orderParameters[i];
+
+                                var orderableColumn = string.IsNullOrEmpty(orderParameter.ColumnName)
+                                    ? resolver.OrderableColumns[orderParameters[i].Column]
+                                    : resolver.OrderableColumns.FirstOrDefault(oc => string.Compare(oc, orderParameter.ColumnName, true) == 0);
+
+                                if (orderableColumn != null)
+                                {
+                                        query = i == 0 ? query.DynamicOrderBy(orderableColumn, isDesc) : query.DynamicThenBy(orderableColumn, isDesc);
+                                }
+                        }
+
+                        return query;
+                }
+
+                public static IQueryable<TData> Search<TData>(this IQueryable<TData> query, DataTableSearch searchParameters, DataTableResolver resolver)
+                    where TData : class
+                {
+                        if (resolver == null)
+                        {
+                                throw new InvalidOperationException("A DataTable resolver is required to search.");
+                        }
+
+                        if (!string.IsNullOrWhiteSpace(searchParameters.Value))
+                        {
+                                return query.DynamicWhere(resolver.SearchableColumn, searchParameters.Value);
+                        }
+
+                        return query;
+                }
         }
-
-        public static IQueryable<TData> Search<TData>(this IQueryable<TData> query, DataTableSearch searchParameters, DataTableResolver resolver)
-            where TData : class
-        {
-            if (resolver == null)
-            {
-                throw new InvalidOperationException("A DataTable resolver is required to search.");
-            }
-
-            if (!string.IsNullOrWhiteSpace(searchParameters.Value))
-            {
-                return query.DynamicWhere(resolver.SearchableColumn, searchParameters.Value);
-            }
-
-            return query;
-        }
-    }
 }
